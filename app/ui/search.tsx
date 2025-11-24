e3eb925f0f636666dc5d53f7c683377dcce3b45a
Mason Karr <karr.mason@gmail.com>
2025-11-24 12:36:04 -0600

    implemented pagination and Server Actions

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,28 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams, useRouter} from 'next/navigation';
+import {useDebouncedCallback} from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
    const searchParams = useSearchParams();
    const pathname = usePathname();
    const { replace } = useRouter();
   
-  function handleSearch(term: string) {
+  
+
+  const handleSearch = useDebouncedCallback ((term) => {
     console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
+    params.set('page','1');
+
     if(term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
       replace(`${pathname}?${params.toString()}`)
-    }
+    }, 900);
   
  
   return (
