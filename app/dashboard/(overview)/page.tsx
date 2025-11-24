f6d46915ee1f3840509534579e142d46d884ee5d
Mason Karr <karr.mason@gmail.com>
2025-11-24 12:56:09 -0600

    fetching card data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,20 +1,15 @@
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {  fetchLatestInvoices , fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, 
+         LatestInvoicesSkeleton, 
+         CardSkeleton 
+        } from '@/app/ui/skeletons';
 
 export default async function Page() {
 
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
 
   return (
     <main>
