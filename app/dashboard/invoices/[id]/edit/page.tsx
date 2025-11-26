cc91c4ee34b875177c0cb0606241d54bed5303e9
Mason Karr <karr.mason@gmail.com>
2025-11-26 15:14:45 -0600

    error handling - try/catch - not-found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,9 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation';
+
+
     export default async function Page(props: { params: Promise<{ id: string }>}) {
     const params = await props.params;
     const id = params.id;
@@ -9,6 +11,10 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+
+    if (!invoice){
+      notFound();
+    }
   return (
     <main>
       <Breadcrumbs
